088bf4836bb2d036cb20f29db8833c4e069fc988
HussainBashaV <hussain.devops0217@gmail.com>
2025-08-14 21:42:30 +0530

    hello world started

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Enter PIN"
-read -s PIN
-echo "Your PIN is $PIN"
+read -s PIN              # -s indicates, hide the pin which you enter
+#echo "Your PIN is $PIN"  # it is just for reference to check pin
