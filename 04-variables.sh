e8b7c75132081621bb4b35239d87fd632cb893dc
HussainBashaV <hussain.devops0217@gmail.com>
2025-08-14 21:33:59 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1 #there should be no space before and after equal (=)
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, How areyou doing?"
+echo "$PERSON2: Hay $PERSON1, I'm doing great, What about you?"
+echo "$PERSON1: That's great to hear you $PERSON2, I'm awsome."
+echo "$PERSON2: When will you come to home $PERSON1?"
+echo "$PERSON1: I'll come in 2days $PERSON2"
