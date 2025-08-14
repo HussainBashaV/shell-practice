4d3777b71bb19a880cfcc6bdfbc890151ae90d57
HussainBashaV <hussain.devops0217@gmail.com>
2025-08-14 21:10:09 +0530

    hello world started

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Basha
+PERSON1=Jeevani
+
+echo "$PERSON1: Hi $PERSON2, How areyou doing?"
+echo "$PERSON2: Hay $PERSON1, I'm doing great, What about you?"
+echo "$PERSON1: that's great to hear you $PERSON2, I'm awsome."
+echo "$PERSON2: when will you come to home $PERSON1?"
+echo "$PERSON1: I'll come in 2days $PERSON2"
