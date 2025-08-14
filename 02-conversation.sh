ba323d88084bc936dbea05b3863c3f9d9ca44ea0
HussainBashaV <hussain.devops0217@gmail.com>
2025-08-14 21:03:48 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo "Basha:: Hi Jeevani, How areyou doing?"
-echo "Jeevani:: Hay Bashu, I'm doing great, What about you?"
-echo "Basha:: that's great to hear you, I'm awsome."
+echo "Jeevani:: Hay Basha, I'm doing great, What about you?"
+echo "Basha:: that's great to hear you Jeevani, I'm awsome."
+echo "Jeevani: when will you come to home Basha?"
+echo "Basha: I'll come in 2days Jeevani"
